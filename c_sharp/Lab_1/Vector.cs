aabbc9af8ac1838b779591c11ff32eb62a231d11
Maxim Dashkovskiy <maxdashk3@gmail.com>
2025-02-02 21:31:39 +0200

    Update Vector.cs

--- c_sharp/Lab_1/Vector.cs
+++ c_sharp/Lab_1/Vector.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
-using static System.Runtime.InteropServices.JavaScript.JSType;
-using static System.Math;
+﻿using static System.Math;
 
 namespace Lab_1
 {
